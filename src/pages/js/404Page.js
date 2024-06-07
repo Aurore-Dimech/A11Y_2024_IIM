e98dfdab5787be1aa69e82c4798b840cb68c0757
Aurore-Dimech <aurore.dimech@edu.devinci.fr>
2024-06-07 20:13:03 +0200

    FINILIZED mainPage 404

--- src/pages/js/404Page.js
+++ src/pages/js/404Page.js
@@ -1,14 +1,29 @@
 import React, { useEffect } from "react";
 
-const NotFoundPage = () => {
+import "../css/404Page.css";
 
+const NotFoundPage = () => {
   useEffect(() => {
-    document.title = "Page introuvable | Bibliothèques de Nice"
-  }, [])
+    document.title = "Page introuvable | Bibliothèques de Nice";
+  }, []);
 
   return (
     <main role="main">
-      <h1>Page introuvable</h1>
+      <section className="unfound-page">
+        <a href="/">flèche retour</a>
+        <div className="main">
+          <div className="section-title">
+            <h2>404</h2>
+          </div>
+          <p>phrase</p>
+        </div>
+          <a
+            href="/"
+            className="button"
+          >
+            accueil
+          </a>
+      </section>
     </main>
   );
 };
