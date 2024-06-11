7a5ba4c48c5023588486f2276c1070ca47628d65
Aurore-Dimech <aurore.dimech@edu.devinci.fr>
2024-06-11 19:43:21 +0200

    ADDED 404 page

--- src/components/js/Footer.js
+++ src/components/js/Footer.js
@@ -113,7 +113,7 @@ const Footer = () => {
         <form>
           <h3>NEWSLETTER</h3>
           <div className="newsletter-container">
-            <label for="newsletter">
+            <label htmlFor="newsletter">
               Votre adresse mail
               <div className="input-newsletter">
                 <input
@@ -141,7 +141,7 @@ const Footer = () => {
                 id="abonnement_newsletter"
                 name="abonnement_newsletter"
               />
-              <label for="abonnement_newsletter">
+              <label htmlFor="abonnement_newsletter">
                 J’accepte de recevoir cette newsletter et je comprends que je
                 peux me désabonner facilement à tout moment.
               </label>
--- src/pages/js/404Page.js
+++ src/pages/js/404Page.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 
 import "../css/404Page.css";
+import image from "../../assets/404.svg";
+import returnArrow from "../../assets/returnArrow.svg";
 
 const NotFoundPage = () => {
   useEffect(() => {
@@ -10,19 +12,25 @@ const NotFoundPage = () => {
   return (
     <main role="main">
       <section className="unfound-page">
-        <a href="/">flèche retour</a>
-        <div className="main">
-          <div className="section-title">
-            <h2>404</h2>
-          </div>
-          <p>phrase</p>
+        <div className="return">
+          <img
+            src={returnArrow}
+            alt=""
+          />
+          <a href="/">Retour à la page d'accueil</a>
         </div>
-          <a
-            href="/"
-            className="button"
-          >
-            accueil
-          </a>
+        <div className="main">
+          <img
+            src={image}
+            alt="Page introuvable, veuillez retourner à la page d'accueil avec le bouton ci-dessous."
+          />
+        </div>
+        <a
+          href="/"
+          className="button"
+        >
+          Accueil
+        </a>
       </section>
     </main>
   );
