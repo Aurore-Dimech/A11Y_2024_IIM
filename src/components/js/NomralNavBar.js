c41c678352ec868a5793d3043101f3574a407d48
Aurore-Dimech <aurore.dimech@edu.devinci.fr>
2024-06-16 22:14:40 +0200

    DELETE useless code

--- src/components/js/NomralNavBar.js
+++ src/components/js/NomralNavBar.js
@@ -1,200 +1,5 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import "../css/NomralNavBar.css";
-
-// function NomralNavBar(clickHandler) {
-//   const [isMobile, setIsMobile] = useState(window.innerWidth < 880);
-
-//   const handleResize = () => {
-//     setIsMobile(window.innerWidth < 880);
-//   };
-
-//   useEffect(() => {
-//     window.addEventListener("resize", handleResize);
-//     return () => {
-//       window.removeEventListener("resize", handleResize);
-//     };
-//   }, []);
-
-//   const SubMenu = ({ isOpen, items }) => {
-//     return (
-//       <ul className={`DetailsList ${isOpen ? "open" : ""}`}>
-//         {items.map((items, index) => (
-//           <li
-//             className="li"
-//             key={index}
-//           >
-//             <a href="/">{items}</a>
-//           </li>
-//         ))}
-//       </ul>
-//     );
-//   };
-
-//   const MenuItems = ({ title, link, subMenuItems }) => {
-//     const [isOpen, setIsOpen] = useState(false);
-//     const handleClick = () => {
-//       setIsOpen(!isOpen);
-//     };
-
-//     const preventDefault = (e) => {
-//       e.preventDefault();
-//     };
-
-//     return (
-//       <li className="List">
-//         {subMenuItems ? (
-//           <div
-//             onClick={handleClick}
-//             className="ListItem"
-//           >
-//             <a
-//               href="/"
-//               onClick={preventDefault}
-//               className="linkForList"
-//             >
-//               {link ? <a href={link}>{title}</a> : title}
-//               {!link && (
-//                 <svg
-//                   className="arrowToBottom"
-//                   role="img"
-//                   aria-label="flèche"
-//                   xmlns="http://www.w3.org/2000/svg"
-//                   width="24"
-//                   height="24"
-//                   viewBox="0 0 24 24"
-//                   fill="none"
-//                 >
-//                   <path
-//                     d="M6 9L12 15L18 9"
-//                     stroke="white"
-//                     strokeWidth="2"
-//                     strokeLinecap="round"
-//                     strokeLinejoin="round"
-//                   />
-//                 </svg>
-//               )}
-//             </a>
-//           </div>
-//         ) : (
-//           <div
-//             onClick={handleClick}
-//             className="ListItem"
-//           >
-//             {link ? <a href={link}>{title}</a> : title}
-//             {!link && (
-//               <svg
-//                 className="arrowToBottom"
-//                 role="img"
-//                 aria-label="flèche"
-//                 xmlns="http://www.w3.org/2000/svg"
-//                 width="24"
-//                 height="24"
-//                 viewBox="0 0 24 24"
-//                 fill="none"
-//               >
-//                 <path
-//                   d="M6 9L12 15L18 9"
-//                   stroke="white"
-//                   strokeWidth="2"
-//                   strokeLinecap="round"
-//                   strokeLinejoin="round"
-//                 />
-//               </svg>
-//             )}
-//           </div>
-//         )}
-
-//         {subMenuItems && (
-//           <SubMenu
-//             isOpen={isOpen}
-//             items={subMenuItems}
-//           />
-//         )}
-//       </li>
-//     );
-//   };
-
-//   const closeNavPrincipale = () => {
-//     if (isMobile) {
-//       document.querySelector(".nav-principal").style.display = "none";
-//       document.querySelector(".CloseIcone").style.display = "flex";
-//     }
-//   };
-
-//   const onClickCloseIcone = () => {
-//     document.querySelector(".nav-principal").style.display = "none";
-//   };
-
-//   return (
-//     <nav
-//       role="navigation"
-//       aria-label="Menu principal"
-//       className="nav-principal"
-//     >
-//       <div className="CloseIcone">
-//         {isMobile ? (
-//           <svg
-//             role="img"
-//             aria-label="flèche"
-//             onClick={onClickCloseIcone}
-//             xmlns="http://www.w3.org/2000/svg"
-//             width="37"
-//             height="37"
-//             viewBox="0 0 37 37"
-//             fill="none"
-//           >
-//             <path
-//               d="M27.75 9.25L9.25 27.75M9.25 9.25L27.75 27.75"
-//               stroke="#FBE11A"
-//               strokeWidth="2"
-//               strokeLinecap="round"
-//               strokeLinejoin="round"
-//             />
-//           </svg>
-//         ) : null}
-//       </div>
-//       <ul className="ListHover">
-//         <MenuItems
-//           title="PRATIQUE"
-//           subMenuItems={["Horaires", "Inscription", "Accueil du groupe"]}
-//         />
-//         <MenuItems
-//           title="BIBLIOTHEQUE"
-//           subMenuItems={[
-//             "Louis Nucéra",
-//             "Romain Gary",
-//             "Bib’Mobile",
-//             "Raoul Mille",
-//             "Bibliothèques de proximité",
-//             "Bibliothèques associées",
-//           ]}
-//         />
-//         <MenuItems
-//           title="AGENDA"
-//           link="/nouveaute"
-//         />
-//         <MenuItems
-//           title="NOUVEAUTE"
-//           link="/nouveaute"
-//         />
-//         <MenuItems
-//           title="EN LIGNE"
-//           link="/en_ligne"
-//         />
-//         <MenuItems
-//           title="DECOUVRIR"
-//           subMenuItems={["Livres", "Cinéma", "Presse", "Musique", "Jeunesse"]}
-//         />
-//         <MenuItems
-//           title="PATRIMOINE"
-//           subMenuItems={["Collections", "Patrimoine Numérisés"]}
-//         />
-//       </ul>
-//     </nav>
-//   );
-// }
-
-// export default NomralNavBar;
 
 function NomralNavBar() {
   const pratiqueRef = useRef();
@@ -263,13 +68,13 @@ function NomralNavBar() {
           {activeMenu === "PRATIQUE" && (
             <ul className="second-chain">
               <li>
-                <a href="…">Horaires</a>
+                <a href="/horaires">Horaires</a>
               </li>
               <li>
-                <a href="…">Inscription</a>
+                <a href="/signup">Inscription</a>
               </li>
               <li>
-                <a href="…">Accueil du groupe</a>
+                <a href="/accueil-groupe">Accueil du groupe</a>
               </li>
             </ul>
           )}
@@ -316,29 +121,29 @@ function NomralNavBar() {
           {activeMenu === "BIBLIOTHEQUE" && (
             <ul className="second-chain">
               <li>
-                <a href="…">Louis Nucéra</a>
+                <a href="/bibliotheque/louis-nucera">Louis Nucéra</a>
               </li>
               <li>
-                <a href="…">Romain Gary</a>
+                <a href="/bibliotheque/romain-gary">Romain Gary</a>
               </li>
               <li>
-                <a href="…">Bib’Mobile</a>
+                <a href="/bibliotheque/bib-mobile">Bib’Mobile</a>
               </li>
               <li>
-                <a href="…">Raoul Mille</a>
+                <a href="/bibliotheque/raoul-mille">Raoul Mille</a>
               </li>
               <li>
-                <a href="…">Bibliothèques de proximité</a>
+                <a href="/bibliotheque/a-proximite">Bibliothèques de proximité</a>
               </li>
               <li>
-                <a href="…">Bibliothèque associées</a>
+                <a href="/bibliotheques/associees">Bibliothèque associées</a>
               </li>
             </ul>
           )}
         </li>
         <li>
           <a
-            href="…"
+            href="/agenda"
             className="button-nav"
           >
             AGENDA
@@ -346,7 +151,7 @@ function NomralNavBar() {
         </li>
         <li>
           <a
-            href="…"
+            href="/nouveaute"
             className="button-nav"
           >
             NOUVEAUTÉS
@@ -354,7 +159,7 @@ function NomralNavBar() {
         </li>
         <li>
           <a
-            href="…"
+            href="/en-ligne"
             className="button-nav"
           >
             EN LIGNE
@@ -402,19 +207,19 @@ function NomralNavBar() {
           {activeMenu === "DECOUVRIR" && (
             <ul className="second-chain">
               <li>
-                <a href="…">Livres</a>
+                <a href="/discover/books">Livres</a>
               </li>
               <li>
-                <a href="…">Cinéma</a>
+                <a href="/discover/cine">Cinéma</a>
               </li>
               <li>
-                <a href="…">Presse</a>
+                <a href="/discover/presse">Presse</a>
               </li>
               <li>
-                <a href="…">Musique</a>
+                <a href="/discover/music">Musique</a>
               </li>
               <li>
-                <a href="…">Jeunesse</a>
+                <a href="/discover/youth">Jeunesse</a>
               </li>
             </ul>
           )}
@@ -463,10 +268,10 @@ function NomralNavBar() {
           {activeMenu === "PATRIMOINE" && (
             <ul className="second-chain">
               <li>
-                <a href="…">Collections</a>
+                <a href="/patrimoine/collections">Collections</a>
               </li>
               <li>
-                <a href="…">Patrimoine numérisée</a>
+                <a href="/patrimoine/num">Patrimoine numérisée</a>
               </li>
             </ul>
           )}
